4eeb67bfcc8afe8939c52bceafb6ff4e984e1559
Jarvis <jarvis-bytes@gmail.com>
2020-11-12 10:41:36 +0800

    update

--- micro-api.sh
+++ micro-api.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # 启动micro api网关 
+# --registry 指定注册中心种类
+# --registry_address 指定注册地址
 micro --registry=etcd --registry_address=127.0.0.1:2379 api --namespace=go.micro --type=api --handler=http
--- micro-web.sh
+++ micro-web.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # 启动微服务监控UI界面，web版，访问地址：http://localhost:8082
+# --registry 指定注册中心种类
+# --registry_address 指定注册地址
 micro --registry=etcd --registry_address=127.0.0.1:2379 web
