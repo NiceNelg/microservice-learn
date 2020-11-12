052e32374eabb072a82a3200175e16b6990f5a6d
Jarvis <jarvis-bytes@gmail.com>
2020-11-12 20:48:31 +0800

    update

--- achievement-srv/server/server.go
+++ achievement-srv/server/server.go
@@ -30,7 +30,8 @@ func main() {
 		micro.Name("go.micro.service.achievement"),
 		micro.Version("lastest"),
 		// 配置nats作为消息中间件，
-		// 这里没有将该服务注册到etcd中，表明消息的订阅只需要自己订阅的事件名称以及接收事件通知的地址，发布者只需要知道事件的名称以及通知地址即可
+		// 这里没有将该服务注册到etcd中，表明消息的订阅只需要知道自己订阅的事件名称以及接收事件通知的地址，
+		// 发布者只需要知道事件的名称以及通知地址即可
 		micro.Broker(
 			nats.NewBroker(
 				broker.Addrs("nats://127.0.0.1:4222"),
